4eac5419a5724b5ff6033521e799ed91c853ee67
VKU CODE <ungurean729@gmail.com>
2024-06-04 18:42:42 +0200

    first comit

---
+++ app/accueil/page.jsx
@@ -0,0 +1,13 @@
+import styles from './accueil.module.scss'
+import Image from 'next/image'
+
+
+export default function Accueil(){
+    return(
+        <>
+            <section>
+                Accueil
+            </section>
+        </>
+    )
+}
---
+++ app/contact/page.jsx
@@ -0,0 +1,13 @@
+import styles from './contact.module.scss'
+import Image from 'next/image'
+
+
+export default function Contact(){
+    return(
+        <>
+            <section>
+
+            </section>
+        </>
+    )
+}
